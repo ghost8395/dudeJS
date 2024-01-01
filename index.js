f647fd7356de8cb7109d437bd03eca107d0f1a15
Manik Mudholkar <manikmudholkar831995@gmail.com>
2024-01-01 20:59:46 +0530

    updated readme

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ sayThatDude('+++++++++++++++++++++++++++++++++++++++')
 sayThatDude('This is from index.js')
 
 // #region sayThatDude/print example
-// for (let count = 0; count < 10; count++) {
-//     sayThatDude('printing count from custom function', count)
-// }
+for (let count = 0; count < 10; count++) {
+    sayThatDude('printing count from custom function', count)
+}
 // #endregion
 
 // #region relaxAndDoItDude example
@@ -14,17 +14,17 @@ sayThatDude('This is from index.js')
 // // #endregionS
 
 // #region nested relaxAndDoItDude example
-let interval = 0;
-let sleep = 2000
-relaxAndDoItLaterDude(sleep, interval, () => {
-    sayThatDude('this is a first callback called at  ' + new Date().toISOString())
-    relaxAndDoItLaterDude(sleep, interval, () => {
-        sayThatDude('this is a second callback called at ' + new Date().toISOString())
-        relaxAndDoItLaterDude(sleep, interval, () => {
-            sayThatDude('this is a third callback called at ' + new Date().toISOString())
-        })
-    })
-})
+// let interval = 0;
+// let sleep = 2000
+// relaxAndDoItLaterDude(sleep, interval, () => {
+//     sayThatDude('this is a first callback called at  ' + new Date().toISOString())
+//     relaxAndDoItLaterDude(sleep, interval, () => {
+//         sayThatDude('this is a second callback called at ' + new Date().toISOString())
+//         relaxAndDoItLaterDude(sleep, interval, () => {
+//             sayThatDude('this is a third callback called at ' + new Date().toISOString())
+//         })
+//     })
+// })
 // #endregionS
 
 // #region setTimeout and setInterval implemntation with relaxAndDoItDude example
